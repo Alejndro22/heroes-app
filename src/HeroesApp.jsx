daf341d4283cbd34d6c37ccb9dd42c936498c1e1
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-07-22 19:17:22 -0600

    Tailwind setup on project

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,7 +1,7 @@
 export const HeroesApp = () => {
   return (
     <>
-      <h1>HeroesApp</h1>
+      <h1 className='text-3xl font-bold underline'>HeroesApp!</h1>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles.css';
+import './index.css';
 import { HeroesApp } from './HeroesApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
