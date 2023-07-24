453e13798cfae265b07f55b0c9701b87ace90cf8
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-07-24 01:32:01 -0600

    Creating cards to display heroes

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,15 +1,17 @@
 import PropTypes from 'prop-types';
 import { getHeroesByPublisher } from '../helpers';
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
   const heroesList = getHeroesByPublisher(publisher);
   return (
     <>
-      <ul>
-        {heroesList?.map((hero) => {
-          return <li key={hero.id}>{hero.superhero}</li>;
-        })}
-      </ul>
+      <div className='grid grid-cols-1 sm:grid-cols-3 place-items-center sm:place-items-start gap-y-3 sm:gap-3 py-4'>
+        {heroesList?.map((hero) => (
+          // send all properties with spread operator
+          <HeroCard key={hero.id} {...hero} />
+        ))}
+      </div>
     </>
   );
 };
