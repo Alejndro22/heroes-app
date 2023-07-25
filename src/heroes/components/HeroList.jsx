50756956274bde05f4c801e6a8f962dd57b454e1
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-07-24 23:30:44 -0600

    useMemo on filter functions to improve performance

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,9 +1,14 @@
+import { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { getHeroesByPublisher } from '../helpers';
 import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
-  const heroesList = getHeroesByPublisher(publisher);
+  // Since assets are static, useMemo could be used to improve performance
+  const heroesList = useMemo(
+    () => getHeroesByPublisher(publisher),
+    [publisher]
+  );
   return (
     <>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center sm:place-items-start gap-y-3 sm:gap-3 py-4'>
