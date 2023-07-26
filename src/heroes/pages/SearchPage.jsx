0a84b14cf6aff7c56809f306f280615a284980ca
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-07-25 19:17:08 -0600

    Show <AlertBanner /> , <Banner /> or <HeroCard /> according to search

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -2,6 +2,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import queryString from 'query-string';
 import { useForm } from '../../hooks/useForm';
 import { HeroCard } from '../components/HeroCard';
+import { AlertBanner, Banner } from '../../ui';
 import { getHeroesByName } from '../helpers/getHeroesByName';
 
 export const SearchPage = () => {
@@ -19,7 +20,7 @@ export const SearchPage = () => {
 
   const onSearchSubmit = (e) => {
     e.preventDefault();
-    if (searchText.trim().length <= 1) return;
+    // if (searchText.trim().length <= 1) return;
 
     // When onSearchSubmit navigate to query parameter path
     navigate(`?q=${searchText}`);
@@ -63,44 +64,18 @@ export const SearchPage = () => {
 
           <hr />
 
-          <div
-            className='flex items-center bg-munsell-blue text-white text-sm font-bold px-4 py-3'
-            role='alert'
-          >
-            <svg
-              className='fill-current w-4 h-4 mr-2'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 20 20'
-            >
-              <path d='M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z' />
-            </svg>
-            <p>Search a hero</p>
-          </div>
+          {q === '' ? (
+            <Banner content={'Search a hero'} />
+          ) : (
+            heroes.length === 0 && (
+              <AlertBanner
+                title={`No hero found with q=${q}`}
+                content={'Please search another hero'}
+              />
+            )
+          )}
 
-          <div
-            className='bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md'
-            role='alert'
-          >
-            <div className='flex'>
-              <div className='py-1'>
-                <svg
-                  className='fill-current h-6 w-6 text-teal-500 mr-4'
-                  xmlns='http://www.w3.org/2000/svg'
-                  viewBox='0 0 20 20'
-                >
-                  <path d='M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z' />
-                </svg>
-              </div>
-
-              <div>
-                <p className='font-bold'>No hero found with q={q}</p>
-
-                <p className='text-sm'>Please search another hero</p>
-              </div>
-            </div>
-          </div>
-
-          <div>
+          <div className='flex flex-col items-center space-y-2'>
             {heroes.map((hero) => (
               <HeroCard key={hero.id} {...hero} />
             ))}
--- src/ui/components/index.js
+++ src/ui/components/index.js
@@ -1,1 +1,3 @@
+export * from './AlertBanner';
+export * from './Banner';
 export * from './Navbar';
