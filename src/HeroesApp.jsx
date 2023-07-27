5961026c770e89af723bd43bbc366bb9fc990603
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-07-26 23:17:13 -0600

    Context created to share global auth, state managed with reducer

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router';
 import { Navbar } from './ui';
+import { AuthProvider } from './auth';
 
 export const HeroesApp = () => {
   return (
-    <>
+    <AuthProvider>
       <Navbar />
       <div className='container mx-auto px-4'>
         <Outlet />
       </div>
-    </>
+    </AuthProvider>
   );
 };
