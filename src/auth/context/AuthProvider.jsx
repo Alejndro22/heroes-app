add2b87a073f6bf89e6609fbf1f8172d08504eda
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-07-27 17:35:15 -0600

    Storage user on localStorage, if this exists reducer takes value

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -3,22 +3,28 @@ import { AuthContext } from './AuthContext';
 import { authReducer } from './authReducer';
 import { types } from '../types/types';
 
-const initialState = {
-  logged: false,
+const init = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  return {
+    logged: !!user,
+    user: user,
+  };
 };
 
 // eslint-disable-next-line react/prop-types
 export const AuthProvider = ({ children }) => {
-  const [authState, dispatch] = useReducer(authReducer, initialState);
+  const [authState, dispatch] = useReducer(authReducer, {}, init);
 
   const login = (name = '') => {
+    const user = { id: 'abc', name };
+
     const action = {
       type: types.login,
-      payload: {
-        id: 'abc',
-        name,
-      },
+      payload: user,
     };
+
+    localStorage.setItem('user', JSON.stringify(user));
 
     dispatch(action);
   };
