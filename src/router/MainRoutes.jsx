6a7c0be739b411c4dd41c58b68d675e15c994372
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-07-27 19:30:49 -0600

    Set private routes
    If auth (user) not found, <Navigate to={'login'}

--- src/router/MainRoutes.jsx
+++ src/router/MainRoutes.jsx
@@ -2,11 +2,16 @@ import { HeroesRoutes } from '../heroes';
 import { LoginPage } from '../auth';
 import { heroesChildrenRoutes } from './HeroesRoutes';
 import ErrorPage from '../heroes/pages/ErrorPage';
+import { PrivateRoutes } from './PrivateRoutes';
 
 export const routes = [
   {
     path: '/',
-    element: <HeroesRoutes />,
+    element: (
+      <PrivateRoutes>
+        <HeroesRoutes />
+      </PrivateRoutes>
+    ),
     errorElement: <ErrorPage />,
     children: heroesChildrenRoutes,
   },
