5742d0f06251a59a0aab5847609bd14730efa7a1
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-07-27 20:06:40 -0600

    Store last path visited, useful for UX
    Value stored on localStorage, whenever user logs out remembers this path before next login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -7,20 +7,23 @@ export const LoginPage = () => {
   const navigate = useNavigate();
 
   const onLogin = () => {
+    const lastPath = localStorage.getItem('lastPath') || '/';
     login('Doug Queme');
 
-    navigate('/', {
+    navigate(lastPath, {
       replace: true,
     });
   };
 
   return (
-    <div className='container p-4'>
-      <h1>LoginPage</h1>
+    <div className='container mx-auto px-4 pt-8'>
+      <h1 className='mb-4 text-4xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl'>
+        LoginPage
+      </h1>
       <hr />
 
       <button
-        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'
+        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded mt-4'
         onClick={onLogin}
       >
         Button
--- src/router/PrivateRoutes.jsx
+++ src/router/PrivateRoutes.jsx
@@ -1,10 +1,18 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../auth';
-import { Navigate } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types
 export const PrivateRoutes = ({ children }) => {
   const { logged } = useContext(AuthContext);
+
+  // useLocation hook returns the current location elements
+  const { pathname, search } = useLocation();
+
+  useEffect(() => {
+    const lastPath = pathname + search;
+    localStorage.setItem('lastPath', lastPath);
+  }, [pathname, search]);
 
   return logged ? children : <Navigate to={'/login'} />;
 };
